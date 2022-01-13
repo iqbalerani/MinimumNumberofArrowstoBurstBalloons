d2c552ceaedec4be99488781133647a13569becf
IQBAL ERANI <iqbalerani@gmail.com>
2022-01-13 23:14:01 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+points = [[10,16],[2,8],[1,6],[7,12]]
+
+class Solution(object):
+    def findMinArrowShots(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        points = sorted(points, key = lambda x: x[1])
+        res, end = 0, -float('inf')
+        for interval in points:
+            if interval[0] > end:
+                res += 1
+                end = interval[1]
+        return res
+
+
+print(Solution().findMinArrowShots(points))
